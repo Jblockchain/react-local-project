425ddc9ea19b675cea8f89c65d6b124b321a4cd5
jatin handa <handashubham12@gmail.com>
2022-10-28 16:17:02 +0530

    react-local-storage

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+// import { FaAlignCenter } from "react-icons/fa";
+import { toast } from "react-toastify";
+import Header from "../Components/Header";
+
+const HomePage = () => {
+  const profilePIcDefault =
+    "https://static.vecteezy.com/system/resources/previews/002/318/271/non_2x/user-profile-icon-free-vector.jpg";
+
+    const [name,setName]= useState(''); 
+    const [email,setEmail]= useState(''); 
+    const [password,setPassword]= useState(''); 
+    const [gender,setGender]= useState(''); 
+    const [img,setImg]= useState(''); 
+    const [terms,setTerms]= useState(false); 
+
+    //bind them
+//form submit handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(name === ""){
+      toast.error("name is require")
+    }else if(email=== "") {
+      toast.error("email is require")
+    }else if(password === "") {
+      toast.error("password is require")
+    }else {
+      localStorage.setItem('name',name)
+      localStorage.setItem('email',email)
+      localStorage.setItem('password',password)
+      localStorage.setItem('img',img)
+      localStorage.setItem('gender',gender)
+      localStorage.setItem('terms',terms)
+
+      toast.success('user saved!')
+    }
+
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container content mt-4">
+        <h5> 📝 Add New User</h5>
+        <div className="row border p-4">
+          <div className="col-md-6">
+            <div className="mb-3">
+              <label htmlFor="exampleInputName" className="form-label">
+                User Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                id="exampleInputName"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email Address
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            {/* radios button inpput ================== */}
+            <div className="d-flex flex-row">
+              Gender :
+              <div className="form-check ms-2">
+                <input
+                  className="form-check-input"
+                  defaultChecked={gender==="Male"}
+                  onClick={e => setGender(e.target.value)}
+                  type="radio"
+                  name="Gender"
+                  value="Male"
+                  id="flexRadioDefault1"
+                />
+                <label className="form-check-label" htmlFor="flexRadioDefault1">
+                  Male
+                </label>
+              </div>
+              <div className="form-check ms-2">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  defaultChecked={gender==="Female"}
+                  onClick={e => setGender(e.target.value)}
+                  name="Gender"
+                  value="Female"
+                  id="flexRadioDefault2"
+                />
+                <label className="form-check-label" htmlFor="flexRadioDefault2">
+                  Female
+                </label>
+              </div>
+            </div>
+            <div className="form-check mt-3">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                checked={terms}
+                onClick={e=> setTerms(e.target.value)}
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                I Acept Terms And Conditions
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="form__submit-btn"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+          {/* col-md-6 ends */}
+
+          <div className="col-md-4 ">
+            <div className="profile_section">
+              <p>Select Profile Picture :</p>
+              <img
+                src={
+                  localStorage.getItem("img")
+                    ? localStorage.getItem("img")
+                    : profilePIcDefault
+                }
+                alt="profile_pic"
+                className="img-thumbnail"
+                height={250}
+                width={250}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="formFile" className="form-label">
+                Default file input example
+              </label>
+              <input className="form-control" type="file" id="formFile" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
